3d589254bab44ac245aedd5fccc9cf3466f0ba29
Joshua Hammer <jebelfor@gmail.com>
2023-10-28 01:27:51 -0500

    Main config

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import { config } from './config/config';
 import router from './router';
-dotenv.config();
+
 
 const app = express();
 const port = config.port || 3000;
+const version = config.version || '/v1';
 
-app.use('/v1', router);
+app.use(version, router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
