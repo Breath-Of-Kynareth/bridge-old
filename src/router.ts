3453f8dafd9f2d4b5143f64cec37522aad7842da
Joshua Hammer <jebelfor@gmail.com>
2023-11-08 20:10:34 -0600

    Remove experiment files

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,8 @@
 import { Router, Request, Response } from 'express';
 import { sayHello } from './endpoints/hello';
-import { auth } from './endpoints/auth';
 import { getRaids, postRaid } from './endpoints/raids';
 const router = Router();
 
-router.get('/hello', sayHello);
-router.get('/auth/:token', auth);
-router.get('/auth/:token', auth);
-router.get('/raids', getRaids);
+router.get('/raids/:application', getRaids);
 router.post('/raids', postRaid);
 export default router;
