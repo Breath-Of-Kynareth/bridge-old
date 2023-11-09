baf1266da7d2b6fa9c6be2341ea2d2236f2aa97a
Joshua Hammer <jebelfor@gmail.com>
2023-11-08 20:10:55 -0600

    Get All Raids

--- src/endpoints/raids.ts
+++ src/endpoints/raids.ts
@@ -1,15 +1,24 @@
 import { Request, Response } from 'express';
 import { perms } from '../auth/perms';
-import { config } from '../config/config';
 import { mongoService } from '../services/mongoService';
+import { authService } from '../services/authService';
 
 export async function getRaids(req: Request, res: Response): Promise<void> {
   const raids = mongoService.getCollections().raids;
   const auth = mongoService.getCollections().auth;
   const authToken = req.headers.authorization;
+  const application = req.params.application
   
   if (authToken === undefined || authToken === null){
-    res.status(403).json({ message: 'Token is required for access to this functionality' })
+    res.status(401).json({ message: 'Token is required for access to this functionality' });
+    return;
+  }
+
+  const permission = await authService.validatePermissions(authToken!, perms.user, application);
+  
+  if(permission === false){
+    res.status(403).json({ message: 'You do not have permission to use this functionality.' });
+    return;
   }
 
   const result = await raids.find({}).toArray();
@@ -17,7 +26,7 @@ export async function getRaids(req: Request, res: Response): Promise<void> {
   if (result) {
     res.json({ raids: result });
   } else {
-    res.status(404).json({ message: 'Could not find authentication with that token.' });
+    res.status(404).json({ message: 'Server is unable to find any Roster Information' });
   }
 }
 
