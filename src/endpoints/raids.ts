892357e704d015d9e6e69de116f11e7fae1250cf
Joshua Hammer <jebelfor@gmail.com>
2023-11-09 23:14:22 -0600

    Endpoint work

--- src/endpoints/raids.ts
+++ src/endpoints/raids.ts
@@ -2,14 +2,22 @@ import { Request, Response } from 'express';
 import { perms } from '../auth/perms';
 import { mongoService } from '../services/mongoService';
 import { authService } from '../services/authService';
+import { Raid } from '../models/raid';
+import { logger } from '../config/logger';
 
-export async function getRaids(req: Request, res: Response): Promise<void> {
+export async function getAllRosters(req: Request, res: Response): Promise<void> {
   const raids = mongoService.getCollections().raids;
   const authToken = req.headers.authorization;
   const application = req.params.application
   
   if (authToken === undefined || authToken === null){
     res.status(401).json({ message: 'Token is required for access to this functionality' });
+    return;
+  }
+
+  const timeoutCheck = await authService.checkTimeout(authToken);
+  if(timeoutCheck === false) {
+    res.status(403).json({ message: 'Authentication Token has expired.' });
     return;
   }
 
@@ -29,7 +37,7 @@ export async function getRaids(req: Request, res: Response): Promise<void> {
   }
 }
 
-export async function postRaid(req: Request, res: Response): Promise<void> {
+export async function postNewRoster(req: Request, res: Response): Promise<void> {
   const raids = mongoService.getCollections().raids;
   const authToken = req.headers.authorization;
   const application = 'Roster-Manager';
@@ -38,18 +46,40 @@ export async function postRaid(req: Request, res: Response): Promise<void> {
     return;
   }
 
-  const permission = await authService.validatePermissions(authToken!, perms.raidLead, application);
+  const timeoutCheck = await authService.checkTimeout(authToken);
+  if(timeoutCheck === false) {
+    res.status(403).json({ message: 'Authentication Token has expired.' });
+    return;
+  }
+
+
+  const permission = await authService.validatePermissions(authToken, perms.raidLead, application);
   
   if(permission === false){
     res.status(403).json({ message: 'You do not have permission to use this functionality.' });
     return;
   }
 
-  const result = await raids.find({}).toArray();
+  console.log(req);
 
-  if (result) {
+  if (!req.body) {
+    res.status(400).json({ error: 'Request body is missing' });
+    return;
+  }
+
+  const newRoster: Raid = req.body;
+
+
+  console.log(newRoster);
+
+  logger.info('Creating New Roster')
+  mongoService.createNewRoster(newRoster);
+
+  res.status(200).json({message: 'Check DB'});
+
+  /*if (result) {
     res.json({ raids: result });
   } else {
     res.status(404).json({ message: 'Server is unable to find any Roster Information' });
-  }
+  }*/
 }
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
-import { Router, Request, Response } from 'express';
-import { getRaids, postRaid } from './endpoints/raids';
+import { Router } from 'express';
+import { getAllRosters, postNewRoster } from './endpoints/raids';
 const router = Router();
 
-router.get('/raids/:application', getRaids);
-router.post('/raids', postRaid);
+router.get('/raids/:application', getAllRosters);
+router.post('/raids', postNewRoster);
 export default router;
