dc4779871fba94ba16dbb14fc0f796d7340a4aab
Joshua Hammer <jebelfor@gmail.com>
2023-11-09 23:14:31 -0600

    Raid Model

--- src/models/raid.ts
+++ src/models/raid.ts
@@ -1,3 +1,18 @@
 export interface Raid{
-    
+    raid: string;
+    date: string;
+    leader: string;
+    dps: Record<string, string>;
+    healers: Record<string, string>;
+    tanks: Record<string, string>;
+    backup_dps: Record<string, string>;
+    backup_healers: Record<string, string>;
+    backup_tanks: Record<string, string>;
+    dps_limit: number;
+    healer_limit: number;
+    tank_limit: number;
+    role_limit: number;
+    memo: string;
 }
+
+// NOTE: Look into mapped types
